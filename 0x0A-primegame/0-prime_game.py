a010624d65081de77aa8e58df0df1c360985e009
Uduak Umanah <eyakenojnr@gmail.com>
2024-12-13 05:51:56 +0100

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,61 +1,42 @@
 #!/usr/bin/python3
 """
-Define isWinner function, a solution to the Prime Game problem
+Define isWineer function, a solution to the Prime Game problem
 """
 
 
-def sieve_of_eratosthenes(n):
+def primes(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
     """
-    Precompute the list of primes up to n using the Sieve of Eratosthenes.
-    Args:
-        n (int): upper boundary of the range.
-    Returns:
-        list: A boolean list where True indicates a prime number.
-    """
+    prime = []
     sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
-    for p in range(2, int(n ** 0.5) + 1):
-        if sieve[p]:
-            for i in range(p * p, n + 1, p):
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
                 sieve[i] = False
-    return sieve
-
-
-def count_primes_up_to(n, sieve):
-    """
-    Count the number of primes up to n using a precomputed sieve.
-    Args:
-        n (int): Upper limit of range.
-        sieve (list): Precomputed sieve of prime numbers.
-    Returns:
-        int: Count of prime numbers up to n.
-    """
-    return sum(sieve[:n + 1])
+    return prime
 
 
 def isWinner(x, nums):
     """
-    Determines winner of Prime Game.
+    Determines winner of Prime Game
     Args:
-        x (int): Number of rounds of the game.
-        nums (list): Upper limits of ranges for each round.
-    Returns:
-        str: Name of the winner ("Maria" or "Ben") or None if there's no winner.
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
     """
-    if not x or not nums:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-
-    max_num = max(nums)
-    sieve = sieve_of_eratosthenes(max_num)
-
     Maria = Ben = 0
-    for num in nums:
-        prime_count = count_primes_up_to(num, sieve)
-        if prime_count % 2 == 0:
+    for i in range(x):
+        prime = primes(nums[i])
+        if len(prime) % 2 == 0:
             Ben += 1
         else:
             Maria += 1
-
     if Maria > Ben:
         return 'Maria'
     elif Ben > Maria:
